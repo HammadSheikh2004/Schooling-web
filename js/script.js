d6e50ac0638777db9a81e5038847736e5862fb16
Hammad Sheikh <hammadsheikh5237@gmail.com>
2022-07-28 22:55:50 +0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .flex .navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+
+window.onscroll = () => {
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+}
+
+
+var swiper = new Swiper(".teacher-slide", {
+    loop:true,
+    spaceBetween: 20,
+    grabCursor:true,
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
